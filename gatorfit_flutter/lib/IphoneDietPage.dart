c9a22a877109b8d8e82a07d681e16558442c9af1
Timothy Macias <147666644+timacias@users.noreply.github.com>
2025-03-02 12:52:43 -0500

    Make an HTTP request to express.js server

--- gatorfit_flutter/lib/IphoneDietPage.dart
+++ gatorfit_flutter/lib/IphoneDietPage.dart
@@ -3,12 +3,55 @@ import 'package:gatorfit_flutter/IphoneExercisePage.dart';
 import 'package:gatorfit_flutter/IphoneLeaderboardPage.dart';
 import 'package:gatorfit_flutter/main.dart';
 
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http.get(
+    Uri.parse('http://localhost:5000/1/attribute/daily_caloric_goal'),
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int user_bmr;
+
+  const Album({required this.user_bmr});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'user_bmr': int user_bmr} => Album(
+        user_bmr: user_bmr,
+      ),
+      _ => throw const FormatException('Failed to load album.'),
+    };
+  }
+}
+
 class IphoneDietPage extends StatefulWidget{
   @override
   _IphoneDietPageState createState() => _IphoneDietPageState();
 }
 
 class _IphoneDietPageState extends State<IphoneDietPage> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
@@ -425,7 +468,7 @@ class _IphoneDietPageState extends State<IphoneDietPage> {
                       TextSpan(
                         children: [
                           TextSpan(
-                            text: '1400',
+                            text: user_bmr,
                             style: TextStyle(
                               color: Colors.black,
                               fontSize: 32,
